268c857ebf07d93ceb8e4177622e79be98c3ae80
hejack0207 <hejack0207@sina.com>
2018-01-15 10:46:12 +0800

    fix

--- cli.py
+++ cli.py
@@ -11,7 +11,7 @@ except ImportError:
     # If you installed this package via pip, you just need to execute this
     from scapy.layers import http
 
-packets = scapy.rdpcap('example_network_traffic.pcap')
+packets = scapy.sniff(filter='host 10.0.51.14',count=4)
 for p in packets:
     print '=' * 78
     p.show()
