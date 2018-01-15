09e48ae49cac7b39b92552a335af0d09ed2315f8
hejack0207 <hejack0207@sina.com>
2018-01-15 12:21:13 +0800

    fix

--- cli.py
+++ cli.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
-try:
-    import scapy.all as scapy
-except ImportError:
-    import scapy
-
-try:
-    # This import works from the project directory
-    import scapy_http.http
-except ImportError:
-    # If you installed this package via pip, you just need to execute this
-    from scapy.layers import http
+import scapy.all as scapy
+from scapy.layers import http
 
 scapy.sniff(filter='host 10.0.51.14',prn=lambda x:x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"))
